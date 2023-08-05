f50e3bc0fd7d55c608218b007ba52f63cba7eab3
syedfarhanpn <syedfarhanpn@gmail.com>
2023-08-06 04:33:12 +0530

    project2 - added all the funtions and made it look better

--- projects/C/mini_projects/phone_book/phone_book.c
+++ projects/C/mini_projects/phone_book/phone_book.c
@@ -7,7 +7,7 @@ struct person
 {
     char name[30];
     char country_code[4];
-    int phone_no;
+    char phone_no[15];
     char sex[6];
     char mail[100];
 };
@@ -35,29 +35,26 @@ void main()
         {
             case 1: list_record();
                     getchar();
-                    getchar();
                     break;
             case 2: add_person();
-                    getchar();
                     getchar();
                     break;
             case 3: search_person();
                     getchar();
-                    getchar();
                     break;
             case 4: remove_person();
-                    getchar();
                     getchar();
                     break;
             case 5: update_person();
                     getchar();
+                    break;
+            case 6: remove_all();
                     getchar();
                     break;
-            default:system("cls");
-                    printf("Thenks for using thE Phone Book");
-                    getchar();
-                    getchar();
-                    exit(1);        
+            case 7: system("cls");
+                    printf("Thanks for using the Phone Book\n");
+                    exit(1);     
+            default:printf("Invalid option try again!\n");
         }
     }
 }
@@ -87,7 +84,7 @@ void add_person()
     if(fp==NULL)
     {
         printf("Error In File Opening,Please Try Again! \n");
-        printf("Press any key to continue....\n");
+        printf("Press Enter to continue....\n");
         return;
     }
     else
@@ -98,8 +95,8 @@ void add_person()
         fflush(stdin);
         fclose(fp);
         system("cls");
-        printf("Record added successfully");
-        printf("Press any key to continue....\n");
+        printf("Record added successfully\n");
+        printf("Press Enter to continue....\n");
     }
 }
 
@@ -114,11 +111,11 @@ void take_input(person *p)
     scanf("%s",p->country_code);
     
     printf("Enter Mobile No : ");
-    scanf("%s",p->phone_no);
+    scanf("%s",&p->phone_no);
     
     printf("Enter sex : ");
     scanf("%s",p->sex);
-    
+
     printf("Enter mail : ");
     scanf("%s",p->mail);
 }
@@ -131,25 +128,26 @@ void list_record()
     if(fp==NULL)
     {
         printf("Error In File Opening,Please Try Again! \n");
-        printf("Press any key to continue....\n");
+        printf("Press Enter to continue....\n");
         return;
     }
     else
     {
         person p;
-        printf("\t\t*********************************************************************\n");
-        printf("\t\t*                   Here Is All Recorded Contacts                   *\n");
-        printf("\t\t*********************************************************************\n");
-        printf("\t\t NAME \t\t\t COUNTRY CODE \t\t PHONE NO \t\t\t SEX \t\t\t MAIL\n");
-        PRINTF("-------------------------------------------------------------------------\n");
+        printf("*********************************************************************************************************************************\n");
+        printf("*                                                 Here Is All Recorded Contacts                                                 *\n");
+        printf("*********************************************************************************************************************************\n");
+        printf("NO\tNAME\t\t\t\tCOUNTRY CODE\tPHONE NO\t\t SEX \t\t MAIL\n");
+        printf("---------------------------------------------------------------------------------------------------------------------------------\n");
         while(fread(&p, sizeof(p), 1, fp)==1)
         {
             int i;
-            int len1 = 40 - strlen(p.name);
-            int len2 = 19 - strlen(p.country_code);
-            int len3 = 15;
-            int len4 = 21 - strlen(p.sex);
-           
+            
+            int len1 = 32 - strlen(p.name);
+            int len2 = 16 - strlen(p.country_code);
+            int len3 = 25 - strlen(p.phone_no);
+            int len4 = 16 - strlen(p.sex);
+
             printf("%s",p.name);
             for(i=0;i<len1;i++)
                 printf(" ");
@@ -157,8 +155,8 @@ void list_record()
             printf("%s",p.country_code);
             for(i=0;i<len2;i++)
                 printf(" ");
-
-            printf("%d",p.phone_no);
+        
+            printf("%s",p.phone_no);
             for(i=0;i<len3;i++)
                 printf(" ");
             
@@ -172,41 +170,44 @@ void list_record()
         }
         fflush(stdin);
         fclose(fp);
-        printf("\n\n Pressany key to continue....\n");
+        printf("\n\n Press Enter to continue....\n");
     }
 }
 
 void search_person()
 {
     system("cls");
-    int phone;
+    char phone[15];
     printf("Enter the phone number of the person you want to find: ");
-    scanf("%d",&phone);
+    scanf("%s",phone);
 
     FILE *fp;
-    fp = open("phonebook_db","rb");
+    fp = fopen("phonebook_db","rb");
     if (fp==NULL)
     {
         printf("Error In File Opening,Please Try Again! \n");
-        printf("Press any key to continue....\n");
+        printf("Press Enter to continue....\n");
         return;
     }
     else
     {
-        int flag=0;
+        int flag=0,l=0;
         person p;
         while(fread(&p,sizeof(p), 1, fp) == 1)
         {
-            if(p.phone_no == phone)
+            l=strcmp(phone,p.phone_no);
+            if(l==0)
             {
-                printf("\t NAME \t\t\t\t COUNTRY CODE \t\t\t\t PHONE NO \t\t\t\t SEX \t\t\t\t MAIL \n");
-                printf("---------------------------------------------------------------------------\n");
+                printf("\n");
+                printf("No\tNAME\t\t\t\tCOUNTRY CODE\tPHONE NO\t\t SEX \t\t MAIL\n");
+                printf("---------------------------------------------------------------------------------------------------------------------------------\n");
                 int i;
-                int len1 = 40 - strlen(p.name);
-                int len2 = 19 - strlen(p.country_code);
-                int len3 = 15;
-                int len4 = 21 - strlen(p.sex);
-            
+                int len1 = 32 - strlen(p.name);
+                int len2 = 16 - strlen(p.country_code);
+                int len3 = 25 - strlen(p.phone_no);
+                int len4 = 16 - strlen(p.sex);
+                
+
                 printf("%s",p.name);
                 for(i=0;i<len1;i++)
                     printf(" ");
@@ -215,7 +216,7 @@ void search_person()
                 for(i=0;i<len2;i++)
                     printf(" ");
 
-                printf("%d",p.phone_no);
+                printf("%s",p.phone_no);
                 for(i=0;i<len3;i++)
                     printf(" ");
                 
@@ -238,6 +239,108 @@ void search_person()
         }
         fflush(stdin);
         fclose(fp);
-        printf("\n\n Pressany key to continue....\n");
+        printf("Press Enter to continue....\n");
     }
+}
+
+void update_person()
+{
+    system("cls");
+    char phone[15];
+    printf("Enter the mobile no of the person you want to update : ");
+    scanf("%s",phone);
+
+    FILE *fp,*temp;
+    fp = fopen("phonebook_db", "rb");
+    temp = fopen("temp", "wb");
+
+    if(fp == NULL)
+    {
+        printf("Error in file opening, Please try again!\n");
+        printf("Press Enter to continue....\n");
+        return;
+    }
+    else
+    {
+        int flag=0,l=0;
+        person p;
+        while(fread(&p, sizeof(p), 1, fp) == 1)
+        {
+            l=strcmp(phone,p.phone_no);
+            if(l==0)
+            {
+                take_input(&p);
+                fwrite(&p, sizeof(p), 1, temp);
+                system("cls");
+                printf("Data updated successfully\n");
+                flag=1;
+            }
+            else
+                fwrite(&p, sizeof(p), 1, temp);
+            fflush(stdin);            
+        }
+        if(flag == 0)
+        {
+            system("cls");
+            printf("No record found on %s number",phone);
+        }
+        fclose(fp);
+        fclose(temp);
+        remove("phonebook_db");
+        rename("temp", "phonebook_db");
+        fflush(stdin);
+        printf("Press Enter to continue....\n");
+    }
+}
+
+void remove_person()
+{
+    system("cls");
+    char phone[15];
+    printf("Enter the mobile number you need to remove : ");
+    scanf("%s", phone);
+    
+    FILE *fp;
+    fp = fopen("phonebook_db", "wb+");
+    
+    if(fp == NULL)
+    {
+        printf("Error in file opening, Please try again!\n");
+        printf("Press Enter to continue....\n");
+        return;
+    }
+    else
+    {
+        int flag=0,l=0;
+        person p;
+        while(fread(&p, sizeof(p), 1, fp) == 1)
+        {
+            l=strcmp(phone,p.phone_no);
+            if(l==0)
+            {
+                // remove("p");
+                system("cls");
+                printf("Person has been removed successfully\n");
+                flag=1;
+            }
+            else continue;           
+        }
+        if(flag == 0)
+        {
+            system("cls");
+            printf("No record found on %s number\n",phone);
+        }
+        fflush(stdin);
+        fclose(fp);
+        printf("Press Enter to continue....\n");
+    }
+}
+
+void remove_all()
+{
+    char choice;
+    system("cls");
+    remove("./phonebook_db");
+    printf("All data in the phonebook deleted successfully\n");
+    printf("Press Enter to continue....\n");
 }
